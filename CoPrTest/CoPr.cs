009e465fd08693fcc00456616048e2bd5e2ca338
Kristijan Cetina <kristijan.cetina@gmail.com>
2015-07-01 13:00:58 +0200

    Revert "te"
    This reverts commit 77a97ccaeea9ef0e5e7a509a428610ff394a9aa0.

--- CoPrTest/CoPr.cs
+++ CoPrTest/CoPr.cs
@@ -12,9 +12,9 @@ namespace CoPrTest
         public static string disableNumberPrintOfTheBarcode = "\x0002110B0\x0003";
         public static string selectLargeImage = "\x0002150010000000\x0003";
         public static string selectMediumImage = "\x0002149010000000\x0003";
-        public static string defineBarcode = "\x000213801380070509809291034\x0003"; //"\x000213801380070509809291033\x0003"; (za karticu razduzivaca 9809291033)
+        public static string defineBarcode = "\x00021380138007050101161213122821029\x0003"; //"\x000213801380070509809291033\x0003"; (za karticu razduzivaca 9809291033)
         public static string defineBarcodeAsCM = "\x00021380150007050101161213122821029\x0003";
-        public static string defineAString = ("\x000212246043032Kartica razduzivaca 1034 ");//("\x000212246043032ULAZ #666 "); //ovako ga nije volja, ok prvi nakon pokretanj app. btn5
+        public static string defineAString = ("\x000212246043032ULAZ #666 ");//("\x000212246043032ULAZ #666 "); //ovako ga nije volja, ok prvi nakon pokretanj app. btn5
         public static string defineAStringAsValidator = ("\x000212258043032ExitBarCode ");
         public static string movementWithPrint = "\x00021611\x0003";
         public static string movementWithPrintRead = "\x000216114\x0003";
